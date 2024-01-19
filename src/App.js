d3f2a5bbbad916fa380c4551dacd214c70249540
me <me@gmail.com>
2024-01-19 11:07:37 +0100

    Tenth commit: React Router Outlet for nested route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 
+const Shop = () => {
+  return (
+    <h1>I'am the shop</h1>
+  )
+}
+
 const App = () => {
   return( 
   <Routes>
-    <Route path = '/home' element={<Home />}></Route>
+    <Route path = '/home' element={<Home />}>
+      <Route path = 'shop' element={<Shop />}></Route>
+    </Route>
+    
   </Routes>
   )
 };
