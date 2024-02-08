bada591bd0274684c70822e6b94a3d66b892f0a3
me <me@gmail.com>
2024-02-08 09:52:07 +0100

    Fhirty-Fiveth Commit: Adding Cart Pt.1

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,12 +1,28 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+//Find if cartItems contain productToAdd
+
+//If found increment quantity
+
+//Return new array with modified cartItems/ new cart item
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+
     const value = { isCartOpen, setIsCartOpen };
     return <CartContext.Provider value={value}> { children } </CartContext.Provider>
 }
