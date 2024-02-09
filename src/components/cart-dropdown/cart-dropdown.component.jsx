a6f08f268cb579a8e637d61d2d2259981ef9d01e
me <me@gmail.com>
2024-02-09 09:14:03 +0100

    Thirty-Eight Commit: Creating Checkout Page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,17 +3,23 @@ import CartItem from '../cart-item/cart-item.component';
 
 import { useContext } from 'react';
 import { CartContext } from '../../context/cart.context';
+import { useNavigate } from 'react-router-dom';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutPageHandler = () => {
+        navigate('/checkout')
+    }
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutPageHandler}>GO TO CHECKOUT</Button>
         </div>
     )
 }
