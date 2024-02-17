8a4fbddc89da2746ff7474e9e5eeedee31dfb244
me <me@gmail.com>
2024-02-17 09:17:31 +0100

    Fifty-Third Commit: Navigation Styled Components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,8 @@
 import { Link, Outlet } from "react-router-dom";
 import { Fragment, useContext } from "react";
 import {ReactComponent as CrownLogo} from '../../assets/crown.svg';
-import'./navigation.styles.scss'
+
+import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles'
 
 import { UserContext } from "../../context/user.context";
 import { CartContext } from "../../context/cart.context";
@@ -23,27 +24,27 @@ const Navigation = () => {
   //console.log(currentUser)
     return(
       <Fragment>
-        <div className="navigation">
-            <Link className="logo-container" to='/'>
+        <NavigationContainer>
+            <LogoContainer to='/'>
                 <CrownLogo className="logo" />
-            </Link>
-            <div className="nav-links-container">
-                <Link className="nav-link" to='/shop'>
+            </LogoContainer>
+            <NavLinks>
+                <NavLink to='/shop'>
                     SHOP
-                </Link>
+                </NavLink>
                 {
                   currentUser ? (
-                    <span className="nav-link" onClick={signOutUser}> SIGN OUT </span>
+                    <NavLink as='span' onClick={signOutUser}> SIGN OUT </NavLink>
                   ) : (
-                    <Link className="nav-link" to='/auth'>
+                    <NavLink className="nav-link" to='/auth'>
                       SIGN IN
-                    </Link>
+                    </NavLink>
                   )
                 }
                 <CartIcon></CartIcon>
-            </div>
+            </NavLinks>
             {isCartOpen && <CartDropdown></CartDropdown>}
-        </div>
+        </NavigationContainer>
         <Outlet />
       </Fragment>
     )
