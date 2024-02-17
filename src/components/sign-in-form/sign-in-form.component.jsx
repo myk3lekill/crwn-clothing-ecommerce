f73061527431225f433cb673aff624607f3ae501
me <me@gmail.com>
2024-02-17 10:18:52 +0100

    Fifty-Fouth Commit: Button Styled Component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -2,7 +2,7 @@ import { createUserDocumentFromAuth, signInWithGooglePopup, signInAuthUserWithEm
 import { useState } from "react"
 import FormInput from '../form-input/form-input.component'
 import './sign-in-form.styles.scss'
-import Button from "../button/button.component"
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component"
 
 
 const defaultFormFields = {
@@ -71,7 +71,7 @@ const SignInForm = () => {
 
                 <div className="buttons-container">
                     <Button type="submit" > Sign In </ Button>
-                    <Button type= 'button' buttonType='google' onClick={signInWithGoogle} > Google Sign In </ Button>
+                    <Button buttonType={BUTTON_TYPE_CLASSES.google} type= 'button' onClick={signInWithGoogle} > Google Sign In </ Button>
                 </div>
             </form>
         </div>
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,7 +2,7 @@ import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "
 import { useState } from "react"
 import FormInput from '../form-input/form-input.component'
 import './sign-up-form.styles.scss'
-import Button from "../button/button.component"
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component"
 
 
 const defaultFormFields = {
@@ -67,7 +67,7 @@ const SignUpForm = () => {
 
                 <FormInput label='confirmPassword' type = 'password' required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
 
-                <Button buttonType='default' type="submit" > Sign Up </ Button>
+                <Button buttonType={BUTTON_TYPE_CLASSES.base} type="submit" > Sign Up </ Button>
             </form>
         </div>
     )
