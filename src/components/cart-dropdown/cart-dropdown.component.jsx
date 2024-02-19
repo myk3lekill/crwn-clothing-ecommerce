7aa027fd62e8063e961dd3b72e55fb9f3f52a575
me <me@gmail.com>
2024-02-19 09:18:37 +0100

    Fifty-Fiveth Commit: Cart Dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,7 @@ import { useContext } from 'react';
 import { CartContext } from '../../context/cart.context';
 import { useNavigate } from 'react-router-dom';
 
-import './cart-dropdown.styles.scss';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -15,12 +15,13 @@ const CartDropdown = () => {
         navigate('/checkout')
     }
     return(
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                
+                {cartItems.length ? cartItems.map(item => <CartItem key={item.id} cartItem={item} />) : (<EmptyMessage>Your Cart is empty</EmptyMessage>)}
+            </CartItems>
             <Button onClick={goToCheckoutPageHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     )
 }
 
