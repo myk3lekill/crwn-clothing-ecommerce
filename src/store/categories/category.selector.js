bc7573a2d1e7acf38dd0cf3267765092507e40c5
me <me@gmail.com>
2024-03-05 18:27:24 +0100

    Seventy-First Commit: Redux Reselect Library

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,32 @@
-export const selectCategoriesMap = (state) => 
-    state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {})
+import { createSelector } from "reselect";
+
+// Initial Selector
+const selectCategoryReducer = (state) => {
+  console.log('Selector 1 Fired')
+  return state.categories
+};
+
+// First Memoized Selector
+export const selectCategories = createSelector(
+  //Input
+    [selectCategoryReducer],
+  //Output
+    (categoriesSlice) => {
+      console.log('Selector 2 Fired')
+      return categoriesSlice.categories 
+    }
+)
+
+export const selectCategoriesMap = createSelector(
+  //Input
+    [selectCategories],
+  //Output
+    (categories) => {     
+      console.log('Selector 3 Fired') 
+      return categories.reduce((acc, category) => {
+        const { title, items } = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {})
+    }
+);
