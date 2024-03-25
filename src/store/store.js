b34483bdd5469c6a100dbc48cb99071f3cd54942
me <me@gmail.com>
2024-03-25 09:18:12 +0100

    Seventy-Fiveth Commit: Redux-Thunk Pt.1

--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,8 @@ import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
+import { thunk } from 'redux-thunk';
+
 const persistConfig = {
     key: 'root',
     storage,
@@ -16,6 +18,12 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+const thunkMiddleware = (store) => (next) => (action) => {
+    if(typeof(action) === 'function') {
+        action(dispatch)
+    }
+}
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
