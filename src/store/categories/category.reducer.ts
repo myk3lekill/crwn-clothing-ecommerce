a5d2c09be351beba7db7720b02ab21a7fa6247a7
me <me@gmail.com>
2024-05-11 08:43:36 +0200

    106th commit: Typing Redux Cart Files Pt.2

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -16,7 +16,7 @@ export const CATEGORIES_INITIAL_STATE = {
     error: null,
 }
 
-export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {} as AnyAction): CategoriesState => {
+export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action: AnyAction): CategoriesState => {
     if(fetchCategoriesStart.match(action)) {
         return {...state, isLoading: true};
     }
