ce0f6e1fabda7cf6f2291c67d50d504edaf3b0a3
me <me@gmail.com>
2024-05-27 17:04:36 +0200

    126th Commit: Updating Components to GraphQl Val.

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -25,10 +25,19 @@ export const CategoriesProvider = ({ children }) => {
   const {loading, error, data} = useQuery(COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log("loading", loading);
-  console.log("data", data);
+useEffect(() => {
+  if(data) {
+    const { collections } = data;
+    const collectionsMap = collections.reduce((acc, collection) => {
+      const { title, items } = collection;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+    setCategoriesMap(collectionsMap)
+  }
+}, [data])
 
-  const value = { categoriesMap };
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
