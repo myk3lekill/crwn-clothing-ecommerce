57bc4f2608f4fc9279be03fef0c60fc75bfadac4
me <me@gmail.com>
2024-05-28 16:40:02 +0200

    129th Commit: CheckoutItem memo optimization

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -5,32 +5,33 @@ import { addItemToCart, clearItemFromCart, removeItemFromCart } from '../../stor
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({cartItem}) => {
-    const { name, imageUrl, price, quantity } = cartItem;
+const CheckoutItem = memo(({cartItem}) => {
+        const { name, imageUrl, price, quantity } = cartItem;
 
-    const cartItems = useSelector(selectCartItems);
+        const cartItems = useSelector(selectCartItems);
 
-    const dispatch = useDispatch();
-    
-    const clearItemHandler = () => dispatch(clearItemFromCart(cartItems, cartItem));
-    const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
-    const removeItemHandler = () => dispatch(removeItemFromCart(cartItems, cartItem));
+        const dispatch = useDispatch();
+        
+        const clearItemHandler = () => dispatch(clearItemFromCart(cartItems, cartItem));
+        const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+        const removeItemHandler = () => dispatch(removeItemFromCart(cartItems, cartItem));
 
-    return (
-        <div className='checkout-item-container'>
-            <div className='image-container'>
-                <img src={imageUrl} alt={`${name}`}></img>
+        return (
+            <div className='checkout-item-container'>
+                <div className='image-container'>
+                    <img src={imageUrl} alt={`${name}`}></img>
+                </div>
+                <span className='name'>{name}</span>
+                <span className='quantity'>
+                    <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
+                        <span className='value'>{quantity}</span>
+                    <div className='arrow' onClick={addItemHandler}>&#10095;</div>
+                </span>
+                <span className='price'>{price}</span>
+                <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
             </div>
-            <span className='name'>{name}</span>
-            <span className='quantity'>
-                <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
-                    <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={addItemHandler}>&#10095;</div>
-            </span>
-            <span className='price'>{price}</span>
-            <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
-        </div>
-    )
-}
+        )
+    }
+)
 
 export default CheckoutItem;
