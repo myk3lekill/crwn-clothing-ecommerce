323df561dcbd6b21e53823ec0763668c5cfe6442
me <me@gmail.com>
2024-05-28 16:32:35 +0200

    128th Commit: CartItem memo optimization

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,19 @@
+import { memo } from 'react';
 import './cart-item.styles.scss';
 
-const CartItem = ({cartItem}) => {
-    const { name, imageUrl, price, quantity } = cartItem
-    return (
-        <div className='cart-item-container'>
-            <img src={imageUrl} alt={`${name}`}></img>
-            <div className='item-details'>
-                <span className='name'>{name}</span>
-                <span className='price'>{quantity} x ${price}</span>
-            </div>
+const CartItem = memo(({cartItem}) => {
+        const { name, imageUrl, price, quantity } = cartItem
+        return (
+            <div className='cart-item-container'>
+                <img src={imageUrl} alt={`${name}`}></img>
+                <div className='item-details'>
+                    <span className='name'>{name}</span>
+                    <span className='price'>{quantity} x ${price}</span>
+                </div>
 
-        </div>
-    )
-}
+            </div>
+        )
+    }
+)
 
 export default CartItem;
