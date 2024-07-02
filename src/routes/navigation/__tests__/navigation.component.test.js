ad6f7e7b067ef71a563ce84b1cc4c39d7ea40527
me <me@gmail.com>
2024-07-02 09:45:31 +0200

    136th Commit: Navigation Tests Pt.2

--- src/routes/navigation/__tests__/navigation.component.test.js
+++ src/routes/navigation/__tests__/navigation.component.test.js
@@ -2,6 +2,8 @@ import { screen } from "@testing-library/react";
 
 import Navigation from "../navigation.component";
 import { renderWithProviders } from "../../../utils/test/test.utils";
+import { setIsCartOpen } from "../../../store/cart/cart.action";
+import { CartItems } from "../../../components/cart-dropdown/cart-dropdown.styles";
 
 describe('Navigation tests', () => {
     test('It should render a Sign In link if there is no currentUser', () => {
@@ -27,4 +29,43 @@ describe('Navigation tests', () => {
     
         expect(screen.queryByText('SIGN IN')).toBeNull();
       });
-})
+
+      test('It should render Sign Out if there is a currentUser', () => {
+        renderWithProviders(<Navigation />, {
+          preloadedState: {
+            user: {
+              currentUser: {},
+            },
+          },
+        });
+    
+        expect(screen.getByText('SIGN OUT')).toBeInTheDocument();
+      });
+
+      test('It should not render cart dropdown if isCartOpen is false', () => {
+        renderWithProviders(<Navigation />, {
+          preloadedState: {
+            cart: {
+              isCartOpen: false,
+              cartItems: [],
+            },
+          },
+        });
+    
+        expect(screen.queryByText('Your Cart is empty')).toBeNull();
+      });
+
+      test('It should render cart dropdown if isCartOpen is true', () => {
+        renderWithProviders(<Navigation />, {
+          preloadedState: {
+            cart: {
+              isCartOpen: true,
+              cartItems: [],
+            },
+          },
+        });
+    
+        expect(screen.getByText('Your Cart is empty')).toBeInTheDocument()
+      });
+      
+      });
