27a1c6efa8c8e61f1dbdc1a4c4b877a8c45f436a
me <me@gmail.com>
2024-07-04 09:17:01 +0200

    138 th Commit: Navigation Test Pt.3

--- src/components/product-card/__tests__/product-card.component.test.js
+++ src/components/product-card/__tests__/product-card.component.test.js
@@ -1,7 +1,11 @@
 import { screen, fireEvent } from "@testing-library/react";
+import * as reactRedux from 'react-redux';
+import * as userAction from "../../../store/user/user.action";
 
 import { renderWithProviders } from "../../../utils/test/test.utils";
 import ProductCard from "../product-card.component";
+import Navigation from "../../../routes/navigation/navigation.component";
+import { signOutStart } from "../../../store/user/user.action";
 
 describe('Product Card tests', () => {
     test('it should add the product item when Product Card button is clicked', async() => {
@@ -22,6 +26,30 @@ describe('Product Card tests', () => {
         await fireEvent.click(addToCartButtonElement);
 
         expect(store.getState().cart.cartItems.length).toBe(1)
+    })
 
+    test('it should dispatch signOutStart action when clickin og the Sign Out link', async() => {
+        const mockDispatch = jest.fn();
+        jest.mock("react-redux", () => ({
+            ...jest.requireActual("react-redux"),
+            useDispatch: () => mockDispatch,
+          }));
+                          
+        renderWithProviders(<Navigation />, {
+            preloadedState: {
+                user: {
+                    currentUser: {email: 'hey@me.com'},
+                    isLoading: false,
+                    error: null
+                }
+            }
+        });
+
+        const signOutLink = screen.getByText(/SIGN OUT/i);
+        expect(signOutLink).toBeInTheDocument();
+        
+        const signOutStartAction = jest.spyOn(userAction, 'signOutStart');
+        await fireEvent.click(signOutLink);
+        expect(signOutStartAction).toHaveBeenCalled();
     })
 })
