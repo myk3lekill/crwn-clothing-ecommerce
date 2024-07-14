3ad120856858addd8959aa4650fbf60fcf5afbc0
me <me@gmail.com>
2024-07-14 10:53:12 +0200

    143th Commit: Category Sagas Pt.2

--- src/store/categories/__tests__/category.saga.test.js
+++ src/store/categories/__tests__/category.saga.test.js
@@ -3,7 +3,9 @@
  */
 
 import { call } from "typed-redux-saga/macro";
-import { testSaga } from "redux-saga-test-plan";
+import { expectSaga, testSaga } from "redux-saga-test-plan";
+import { getCategoriesAndDocuments } from "../../../utils/firebase/firebase.utils";
+import { throwError } from "redux-saga-test-plan/providers";
 
 //SOLVE TextDecoder error:
 import { TextEncoder, TextDecoder } from 'util'
@@ -13,6 +15,7 @@ global.TextDecoder = TextDecoder
 
 import { categoriesSaga, onFetchCategories, fetchCategoriesAsync } from "../category.saga";
 import { CATEGORIES_ACTION_TYPES } from "../category.types";
+import { fetchCategoriesFailed, fetchCategoriesSuccess } from "../category.action";
 
 describe('category saga', () => { 
     test('categorySaga', () => {
@@ -23,6 +26,7 @@ describe('category saga', () => {
         .next()
         .isDone()
     })
+
     test('onFetchCategories', () => {
         testSaga(onFetchCategories)
         .next()
@@ -32,5 +36,29 @@ describe('category saga', () => {
         )
         .next()
         .isDone()
+    })
+
+    test('fetchCategoriesAsync success', () => {
+        const mockCategoriesArray = [
+            {id: 1, name: 'Category 1'},
+            {id: 2, name: 'Category 2'},
+
+        ]
+        return expectSaga(fetchCategoriesAsync)
+        .provide([
+            [call(getCategoriesAndDocuments), mockCategoriesArray] //use the mockCategoriesArray instead of the call(getCategoriesAndDocuments effect)
+        ])
+        .put(fetchCategoriesSuccess(mockCategoriesArray)) //put fetchCategoriesSuccess method passign in the mockCategoriesArray
+        .run()
+    })
+
+    test('fetchCategoriesAsync failure', () => {
+        const mockError = new Error('An error occured')
+        return expectSaga(fetchCategoriesAsync)
+        .provide([
+            [call(getCategoriesAndDocuments), throwError(mockError)]
+        ])
+        .put(fetchCategoriesFailed(mockError))
+        .run()
     })
 })
